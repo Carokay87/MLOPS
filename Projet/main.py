442d5a968282688bc1fff6905e74243888d9353b
victto <ViCToRia666>
2022-12-13 16:41:33 +0100

    add main

--- Projet/main.py
+++ Projet/main.py
@@ -1,20 +1,26 @@
-
-#Basic falsk serveur
+# Basic falsk serveur
 from flask import Flask, render_template, request
+
+server_API = "http://localhost:5001"
+
+import requests
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+	return render_template("index.html")
+
 
 @app.route("/uploader", methods=["POST"])
 def uploader():
-    f = request.files["file"]
-    f.save(f.filename)
-    
-    
+	f = request.files["file"]
+	prediction: requests.Response = requests.post(server_API + "/analyse", files={"file": f})
+	
+	print(prediction.json()["genre"])
+	return prediction.json()["genre"]
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+	app.run(debug=True)
